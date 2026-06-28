85441d398fc6fcd0fbe9255e77a2e09c9f66f24a
shashwat3115 <52856578+shashwat3115@users.noreply.github.com>
2026-06-28 22:50:47 +0530

    Update blink.ino

--- Week 1/Q-4 Blink/blink.ino
+++ Week 1/Q-4 Blink/blink.ino
@@ -1,9 +1,12 @@
-//v1.0
+//v1.1
+//add serial blink counter output
 
 const int LED_PIN = 13;
+int blinkCount = 0;
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -12,4 +15,8 @@ void loop() {
 
   digitalWrite(LED_PIN, LOW);
   delay(500);
+
+  blinkCount++;
+  Serial.print("Blink count: ");
+  Serial.println(blinkCount);
 }
